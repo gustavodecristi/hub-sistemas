947ae9739636627b9c055d62fe469d9a945a9e19
Gustavo de Cristi Bidoia <gustavo.bidoia@unimedmarilia.com.br>
2026-06-01 17:22:20 -0300

    Novo sistema

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const sistemas = document.querySelectorAll(".system-icon");
-const input = document.getElementById('busca-input')
+const sistemas = document.querySelectorAll(".system-icon")
+const input = document.getElementById("busca-input")
 
 input.addEventListener("input", (ev) => {
-    const busca = ev.target.value.toLowerCase();
+    const busca = ev.target.value.toLowerCase()
 
     sistemas.forEach(sistema => {
-        const nome = sistema.querySelector("p").textContent.toLowerCase();
+        const nome = sistema.querySelector("p").textContent.toLowerCase()
 
         if (nome.includes(busca)) {
             sistema.style.display = ''
